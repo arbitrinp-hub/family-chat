3face37b162aa2f62c50daeae3686a4ffab22ac0
arbitrinp-hub <arbitrinp@gmail.com>
2026-05-28 23:11:03 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-const CACHE = 'family-messenger-v3';
-const ASSETS = ['/', '/index.html', '/app.js', '/style.css'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
